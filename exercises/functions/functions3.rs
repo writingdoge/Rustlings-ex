654a7ba6ac3f62b5bdad416cacab4a2f690e8952
rrpan <rrpan@mails.ccnu.edu.cn>
2025-01-16 13:22:06 +0800

    update

--- exercises/functions/functions3.rs
+++ exercises/functions/functions3.rs
@@ -3,13 +3,12 @@
 // Execute `rustlings hint functions3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
-    call_me();
+    call_me(0);
 }
 
-fn call_me(num: u32) {
+fn call_me(num: u8) {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,14 +13,21 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(n:i32) -> i32 {
+    if n<=40{
+        2*n
+    }
+    else{
+        n
+    }
+ } 
 
 // Don't modify this function!
-#[test]
-fn verify_test() {
+    #[test]
+    fn verify_test() {
     let price1 = calculate_price_of_apples(35);
     let price2 = calculate_price_of_apples(40);
     let price3 = calculate_price_of_apples(41);
